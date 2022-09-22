4ace6af3d38a4866e951bd21aeee8d02e675ea50
raghibnsl <raghibnsl@github.com>
2022-09-22 17:57:09 +0600

    flask_web_thurs

--- flask/web_site/auth.py
+++ flask/web_site/auth.py
@@ -1,4 +1,9 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, flash,redirect, url_for
+from .models import User
+from werkzeug.security import generate_password_hash, check_password_hash
+
+
+
 auth=Blueprint('auth',__name__)
 
 @auth.route('/login',methods=["GET","POST"])
@@ -15,8 +20,22 @@ def logout():
 def signup():
     if request.method=="POST":
         email=request.form.get('email')
-        first_name=request.form.get('firstName')
+        firstname=request.form.get('firstName')
         password1=request.form.get('password3')
         password2=request.form.get('password4')
-        print(email,first_name,password1, password2)
+        if len(email)<10:
+            flash("Email is too short", category="error")
+        elif " " in email:
+            flash("Avoid using blankspaces in emails",category='error')
+        elif len(password1)<8:
+            flash("The password must be atleast of 8 characters in length", category='error')
+        elif password2!=password1:
+            flash("Sorry your passwords dont match",category="error")
+        else:
+            new_user=User(email=email,password=generate_password_hash(password1,method='sha256'),first_name=firstname)
+            db.session.add(new_user)
+            db.session.commit()
+            flash("Account Created!",category='success')
+            return redirect(url_for('views.home'))
+
     return render_template("signup.html")
